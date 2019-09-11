dfa3800b5e4cd60032baa4304de52aac36aa6fee
lgou2w <lgou2w@hotmail.com>
2019-09-11 11:20:19 +0800

    Add nbt-node.ts, todo file

---
+++ src/nbt-node.ts
@@ -0,0 +1,14 @@
+// TODO Packaging problem; Target -> Node.js
+
+import * as fs from 'fs';
+import * as path from 'path';
+import { NBT } from './nbt'
+import { read } from './nbt-reader'
+
+export function readFile(file: string, littleEndian?: boolean): NBT {
+    let resolveFile = path.resolve(file);
+    let buffer = fs.readFileSync(resolveFile);
+    return read(new Int8Array(buffer), littleEndian);
+}
+
+// TODO: writeFile, gzip, pako etc..
