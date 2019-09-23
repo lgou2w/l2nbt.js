7a23e0fa7e168bcb3601ce25f325e1c48d0ad760
lgou2w <lgou2w@hotmail.com>
2019-09-23 16:37:25 +0800

    Mojangson unit test passed

--- test/test-json.ts
+++ test/test-json.ts
@@ -15,40 +15,42 @@ import {
 } from '../src/nbt';
 import {
     toMojangson,
-    parseMojangson,
-    parseMojangsonValue
+    parseMojangson
 } from '../src/mojangson'
 
 describe('NBT to mojangson', () => {
-    it('should to mojangson', async () => {
-        let nbt = tagCompound({
-            byte: tagByte(127),
-            short: tagShort(25600),
-            int: tagInt(14586233),
-            long: tagLong('2697354127323'),
-            float: tagFloat(1.256977),
-            double: tagDouble(3.1415926535),
-            string: tagString("Hello World"),
-            list: tagList([ tagByte(1), tagByte(2) ]),
-            barr: tagByteArray([0, 1, 2, 3]),
-            iarr: tagIntArray([0, 1, 2, 3]),
-            larr: tagLongArray([0, 1, 2, 3]),
-            tag: tagCompound({
-                display: tagCompound({
-                    Name: tagString('Diamond Sword')
-                })
-            })
-        });
-        let mojangson = toMojangson(nbt);
-        console.log(mojangson); // TODO expect
+    it('should equal', async () => {
+        expect(toMojangson(tagByte(123))).to.equal('123b'); // byte : suffix = b
+        expect(toMojangson(tagShort(456))).to.equal('456s'); // short : suffix = s
+        expect(toMojangson(tagInt(123456))).to.equal('123456'); // int : no suffix
+        expect(toMojangson(tagLong(233))).to.equal('233L'); // long : suffix = L  Uppercase!
+        expect(toMojangson(tagFloat(1.2456))).to.equal('1.2456f'); // float : suffix = f
+        expect(toMojangson(tagDouble(3.14159))).to.equal('3.14159d'); // double : suffix = d
+        expect(toMojangson(tagString('Hello'))).to.equal("\"Hello\""); // string : escape double quotes
+        expect(toMojangson(tagByteArray([0, 1, 2]))).to.equal('[B;0B,1B,2B]'); // byte array
+        expect(toMojangson(tagIntArray([0, 1, 2]))).to.equal('[I;0,1,2]'); // int array
+        expect(toMojangson(tagLongArray([0, 1, 2]))).to.equal('[L;0L,1L,2L]'); // long array
+        expect(toMojangson(tagList([tagByte(1), tagByte(2)]))).to.equal('[1b,2b]'); // list
+        expect(toMojangson(tagCompound({ foo: tagByte(1) }))).to.equal('{\"foo\":1b}');
+        expect(toMojangson(tagCompound({}))).to.equal('{}');
     });
 });
 
 describe('Mojangson to NBT', () => {
-    it('should to NBT', async () => {
-        let mojangson = '{"byte":127b,"short":25600s,"int":14586233,"long":2697354127323L,"float":1.256977f,"double":3.1415926535d,"string":"Hello World","list":[1b,2b],"barr":[B;0B,1B,2B,3B],"iarr":[I;0,1,2,3],"larr":[L;0L,1L,2L,3L],"tag":{"display":{"Name":\n' +
-            '"Diamond Sword"}}}';
+    it('should std', async () => {
+        let mojangson = '{"byte":127b,"short":25600s,"int":14586233,"long":2697354127323L,"float":1.256977f,"double":3.1415926535d,"string":"Hello World","list":[1b,2b],"barr":[B;0B,1B,2B,3B],"iarr":[I;0,1,2,3],"larr":[L;0L,1L,2L,3L],"tag":{"display":{"Name":"Diamond Sword"}}}';
         let compound = parseMojangson(mojangson);
-        console.log(toMojangson(compound)); // TODO expect
+        expect(compound.byte).to.equal(127);
+        expect(compound.short).to.equal(25600);
+        expect(compound.int).to.equal(14586233);
+        expect(compound.long).to.equal(BigInt('2697354127323'));
+        expect(compound.float).to.equal(1.256977);
+        expect(compound.double).to.equal(3.1415926535);
+        expect(compound.string).to.equal('Hello World');
+        expect(compound.list).to.have.same.members([1, 2]);
+        expect(compound.barr).to.have.same.members([0, 1, 2, 3]);
+        expect(compound.iarr).to.have.same.members([0, 1, 2, 3]);
+        expect(compound.larr).to.have.same.members([BigInt(0), BigInt(1), BigInt(2), BigInt(3)]);
+        expect(compound.tag.display.Name).to.equal('Diamond Sword');
     });
 });
