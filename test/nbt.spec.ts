864cbde94faa4ccf02dba42f06b24a8d5dab9d7e
lgou2w <lgou2w@hotmail.com>
2020-05-10 23:44:57 +0800

    test: add nbt.ts basic unit test

--- test/nbt.spec.ts
+++ test/nbt.spec.ts
@@ -1,24 +1,123 @@
+/* eslint-disable no-unused-expressions */
+
 import { expect } from 'chai'
 import {
-  NBTTypes
-  // tagByte
-  // tagShort,
-  // tagInt,
-  // tagLong,
-  // tagFloat,
-  // tagDouble,
-  // tagByteArray,
-  // tagString,
-  // tagList,
-  // tagCompound,
-  // tagIntArray,
-  // tagLongArray
+  NBTTypes,
+  tagByte,
+  tagShort,
+  tagInt,
+  tagLong,
+  tagFloat,
+  tagDouble,
+  tagByteArray,
+  tagString,
+  tagList,
+  tagCompound,
+  tagIntArray,
+  tagLongArray
 } from '../src'
 
+const PROPERTY_TYPE = '__type__'
+const PROPERTY_VALUE = '__value__'
+
 describe('l2nbt.js - nbt.ts unit test', () => {
-  describe('NBTType', () => {
+  describe('0x1 - tagByte', () => {
     it('basic', () => {
-      expect(NBTTypes.TAG_BYTE).to.be.eq(0x1)
+      const tag = tagByte(1)
+      expect(tag).to.have.property(PROPERTY_TYPE, NBTTypes.TAG_BYTE)
+      expect(tag).to.have.property(PROPERTY_VALUE).to.be.eq(1)
+    })
+  })
+  describe('0x2 - tagShort', () => {
+    it('basic', () => {
+      const tag = tagShort(1)
+      expect(tag).to.have.property(PROPERTY_TYPE, NBTTypes.TAG_SHORT)
+      expect(tag).to.have.property(PROPERTY_VALUE).to.be.eq(1)
+    })
+  })
+  describe('0x3 - tagInt', () => {
+    it('basic', () => {
+      const tag = tagInt(1)
+      expect(tag).to.have.property(PROPERTY_TYPE, NBTTypes.TAG_INT)
+      expect(tag).to.have.property(PROPERTY_VALUE).to.be.eq(1)
+    })
+  })
+  describe('0x4 - tagLong', () => {
+    it('basic', () => {
+      const tag = tagLong(1)
+      expect(tag).to.have.property(PROPERTY_TYPE, NBTTypes.TAG_LONG)
+      expect(tag).to.have.property(PROPERTY_VALUE).to.be.eq(BigInt(1))
+    })
+  })
+  describe('0x5 - tagFloat', () => {
+    it('basic', () => {
+      const tag = tagFloat(1)
+      expect(tag).to.have.property(PROPERTY_TYPE, NBTTypes.TAG_FLOAT)
+      expect(tag).to.have.property(PROPERTY_VALUE).to.be.eq(1)
+    })
+  })
+  describe('0x6 - tagDouble', () => {
+    it('basic', () => {
+      const tag = tagDouble(1)
+      expect(tag).to.have.property(PROPERTY_TYPE, NBTTypes.TAG_DOUBLE)
+      expect(tag).to.have.property(PROPERTY_VALUE).to.be.eq(1)
+    })
+  })
+  describe('0x7 - tagByteArray', () => {
+    it('basic', () => {
+      const tag = tagByteArray([1])
+      expect(tag).to.have.property(PROPERTY_TYPE, NBTTypes.TAG_BYTE_ARRAY)
+      expect(tag)
+        .to.have.property(PROPERTY_VALUE)
+        .that.have.same.members([1])
+        .with.lengthOf(1)
+    })
+  })
+  describe('0x8 - tagString', () => {
+    it('basic', () => {
+      const tag = tagString('1')
+      expect(tag).to.have.property(PROPERTY_TYPE, NBTTypes.TAG_STRING)
+      expect(tag)
+        .to.have.property(PROPERTY_VALUE)
+        .to.be.eq('1')
+    })
+  })
+  describe('0x9 - tagList', () => {
+    it('basic', () => {
+      const tag = tagList()
+      expect(tag).to.have.property(PROPERTY_TYPE, NBTTypes.TAG_LIST)
+      expect(tag)
+        .to.have.property(PROPERTY_VALUE)
+        .that.is.empty
+    })
+  })
+  describe('0xA - tagCompound', () => {
+    it('basic', () => {
+      const tag = tagCompound()
+      expect(tag).to.have.property(PROPERTY_TYPE, NBTTypes.TAG_COMPOUND)
+      expect(tag)
+        .to.have.property(PROPERTY_VALUE)
+        .that.is.empty
+    })
+  })
+  describe('0xB - tagIntArray', () => {
+    it('basic', () => {
+      const tag = tagIntArray([1])
+      expect(tag).to.have.property(PROPERTY_TYPE, NBTTypes.TAG_INT_ARRAY)
+      expect(tag)
+        .to.have.property(PROPERTY_VALUE)
+        .that.have.same.members([1])
+        .with.lengthOf(1)
+    })
+  })
+  describe('0xC - tagLongArray', () => {
+    it('basic', () => {
+      const tag = tagLongArray([1])
+      expect(tag).to.have.property(PROPERTY_TYPE, NBTTypes.TAG_LONG_ARRAY)
+      expect(tag)
+        .to.have.property(PROPERTY_VALUE)
+        .that.have.same.members([BigInt(1)])
+        .with.lengthOf(1)
     })
   })
 })
